e30d8f8f4ec94bf550326ee7b0105fd0d94308f8
shuukouha <shuukouha@192.168.100.16>
2017-09-15 10:17:20 +0900

    some test for google storage and datastore.save here more than throw away

---
+++ java-test/src/DataStorageTest.java
@@ -0,0 +1,113 @@
+package src;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+import org.springframework.cloud.gcp.storage.GoogleStorageResource;
+import org.springframework.core.io.Resource;
+
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+
+public class DataStorageTest {
+	private static final String TEST_CONTENT = "My preferred option in this circumstance is to use org.apache.commons.codec.binary.Hex which has useful APIs for converting between Stringy hex and binary. For example:\n"
+			+ "Hex.decodeHex(char[] data) which throws a DecoderException if there are non-hex characters in the array, or if there are an odd number of characters.\n"
+			+ "Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].\n"
+			+ "Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].\n"
+			+ "Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].\n"
+			+ "Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].\n"
+			+ "Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].\n"
+			+ "Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].\n"
+			+ "Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].\n"
+			+ "Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].\n"
+			+ "Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].\n"
+			+ "Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].\n"
+			+ "Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].\n"
+			+ "Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].\n"
+			+ "Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].\n"
+			+ "Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].\n"
+			+ "Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].\n"
+			+ "Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].\n"
+			+ "Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].\n"
+			+ "Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].Hex.encodeHex(byte[] data) is the counterpart to the decode method above, and spits out the char[].\n";
+
+	@Test
+	public void test1() throws Exception {
+		Storage storage = StorageOptions.getDefaultInstance().getService();
+		String inputPath = String.format("gs://%s/backup/%s", "syuu-test", "masterTravelForm-*.csv");
+		Resource resource = new GoogleStorageResource(storage, inputPath);
+		System.out.println(resource.getFilename());
+	}
+
+	@Test
+	public void test2() throws Exception {
+		int testThreadNum = 7;
+		int fileSize = 100;
+		int fileNumberPerThread = 1;
+
+		Storage storage = StorageOptions.getDefaultInstance().getService();
+		CountDownLatch start = new CountDownLatch(1);
+		CountDownLatch finish = new CountDownLatch(testThreadNum);
+
+		IntStream.rangeClosed(1, testThreadNum).forEach(i -> {
+			Action action = new Action(storage, "ActionThread" + i, start, finish, fileNumberPerThread, fileSize);
+			action.start();
+		});
+
+		start.countDown();
+		finish.await();
+		System.out.println("finished!");
+	}
+
+	public static class Action extends Thread {
+		private Storage storage;
+		private String name;
+
+		private CountDownLatch start;
+		private CountDownLatch finish;
+		private int fileNumber;
+		private int fileSize;
+
+		public Action(Storage storage, String name, CountDownLatch start, CountDownLatch finish, int fileNumber,
+				int fileSize) {
+			this.storage = storage;
+			this.name = name;
+			this.start = start;
+			this.finish = finish;
+			this.fileNumber = fileNumber;
+			this.fileSize = fileSize;
+		}
+
+		public void run() {
+			try {
+				start.await();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			IntStream.rangeClosed(1, fileNumber).forEach(i -> {
+				BlobInfo blobInfo = BlobInfo
+						.newBuilder(BlobId.of("syuu-test", "test/" + name + "_" + i)).build();
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				
+				while (out.size() / (1024 * 1024) < fileSize) {
+					System.out.println("size:" + out.size() / (1024 * 1024) + "MB");
+					try {
+						out.write(TEST_CONTENT.getBytes());
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				storage.create(blobInfo, out.toByteArray());
+				System.out.println(name + "create file! " + i);
+			});
+			finish.countDown();
+		}
+	}
+}
---
+++ java-test/src/DataStoreTest.java
@@ -0,0 +1,84 @@
+package co.jp.topgate.migration;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.stream.IntStream;
+
+import org.junit.Test;
+
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Query;
+import com.google.cloud.datastore.QueryResults;
+
+public class DataStoreTest {
+
+	@Test
+	public void test1() throws Exception {
+
+		Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+		String gqlQuery = "select __key__ from Test where __key__ = Key(Test, '001', Test, 11)";
+		Query<?> query = Query.newGqlQueryBuilder(gqlQuery).setAllowLiteral(true).build();
+		QueryResults<?> results = datastore.run(query);
+		if (!results.hasNext())
+			System.out.println("no value in datastore");
+	}
+
+	@Test
+	public void test2() throws Exception {
+		int testThreadNum = 10;
+
+		Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+		CountDownLatch start = new CountDownLatch(1);
+		CountDownLatch finish = new CountDownLatch(testThreadNum);
+
+		IntStream.rangeClosed(1, testThreadNum).forEach(i -> {
+			Action action = new Action(datastore, "ActionThread" + i, start, finish);
+			action.start();
+		});
+
+		start.countDown();
+		finish.await();
+		System.out.println("finished!");
+	}
+
+	public static class Action extends Thread {
+		private Datastore datastore;
+
+		private String name;
+
+		private CountDownLatch start;
+
+		private CountDownLatch finish;
+
+		public Action(Datastore datastore, String name, CountDownLatch start, CountDownLatch finish) {
+			this.datastore = datastore;
+			this.name = name;
+			this.start = start;
+			this.finish = finish;
+		}
+
+		public void run() {
+			try {
+				start.await();
+			}
+			catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			IntStream.rangeClosed(1, 1000).forEach(i -> {
+				// String gqlQuery = "select __key__ from MasterTravelForm where __key__
+				// =Key(MasterTravelFormList, '0000030001', MasterTravelForm, "
+				// + i + ")";
+				String gqlQuery = "select __key__ from Test where __key__ =Key(Test, '001', Test, 11)";
+				Query<?> query = Query.newGqlQueryBuilder(gqlQuery).setAllowLiteral(true).build();
+				QueryResults<?> results = datastore.run(query);
+				System.out.println("name:" + name + ";  i:" + i);
+				if (!results.hasNext())
+					System.out.println("no value in datastore");
+
+			});
+			finish.countDown();
+		}
+	}
+}
